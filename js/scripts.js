c936c3a67132902e1cd00d8ecd02448cd7760079
Steffonesc <78707184+Steffonesc@users.noreply.github.com>
2021-02-24 10:40:32 -0600

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,8 @@ console.log("Hi Welcome to my Portfolio site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-}
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
-  }
 }
